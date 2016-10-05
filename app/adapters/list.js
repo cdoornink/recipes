47b0c61f2185da97faeb26935b3aa4ea722b3d4c
Chris Doornink <cdoornink@rhapsody.com>
2016-10-05 11:04:45 -0700

    dev database added, aisleDesignations created as a util

---
+++ app/adapters/list.js
@@ -0,0 +1,12 @@
+import ApplicationAdapter from './application';
+import config from '../config/environment';
+
+export default ApplicationAdapter.extend({
+  pathForType: function(type) {
+    let path = 'lists'
+    if (config.environment == 'development') {
+      path = "devvingLists"
+    }
+    return path
+  }
+});
---
+++ tests/unit/utils/aisle-designations-test.js
@@ -0,0 +1,10 @@
+import aisleDesignations from '../../../utils/aisle-designations';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | aisle designations');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = aisleDesignations();
+  assert.ok(result);
+});
